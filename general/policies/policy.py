58779bb22787eec9f7fc625631adcbf0af54a162
eito-fis <eamon@itofisher.com>
2019-11-01 14:07:21 -0700

    Very simple base policy with literally nothing in it created

---
+++ general/policies/policy.py
@@ -0,0 +1,12 @@
+import numpy as np
+import tensorflow as tf
+
+class Policy():
+    """
+    General policy framework
+    """
+    def __init__(self):
+        pass
+    
+    def step(self):
+        return NotImplementedError
