c081cc9bf21d4ec38ba3242c56c725298dc152e8
eito-fis <eamon@itofisher.com>
2019-11-08 15:08:50 -0800

    Fixed return statement

--- src/general/replay_buffers/replay_buffer.py
+++ src/general/replay_buffers/replay_buffer.py
@@ -71,8 +71,8 @@ class ReplayBuffer(object):
             rewards.append(reward)
             obses_tp1.append(np.array(obs_tp1, copy=False))
             dones.append(done)
-        return np.array(obses_t), np.array(actions), np.array(rewards),
-    np.array(obses_tp1), np.array(dones)
+        return np.array(obses_t), np.array(actions), np.array(rewards),\
+                np.array(obses_tp1), np.array(dones)
 
     def sample(self, batch_size, **_kwargs):
         """
