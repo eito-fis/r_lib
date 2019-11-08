aa7d63b3b880e8fdab6a91049582ed62b5294245
eito-fis <eamon@itofisher.com>
2019-11-07 18:13:00 -0800

    Old SAC model removed, new SAC model updated with documentation and changed to extend general Model class for saving capabilitiees

--- sac/sac_model.py
+++ sac/sac_model.py
@@ -3,13 +3,42 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
 
-class SACQNet(tf.keras.model.Model):
+from src.general.models.model import Model
+
+class SACQNet(Model):
+    """
+    Soft Actor Critic Q Model
+
+    Attributes:
+        state_size: State size the model will accept
+        convs: Convolutional layers of the model
+        flatten: Flatten operation of the model
+        fc: List containing the actor's fully connected layers
+        out: Output layer of the actor
+    """
     def __init__(self,
                  state_size=None,
                  stack_size=None,
                  action_space=None,
                  fc=None,
                  conv_size=None):
+        """
+        Constructor.
+
+        Parameters:
+            state_size: List containing the expected size of the state
+            stack_size: Numer of states we can expect in our stack
+            action_space: Box action space the actor will work in
+            fc: Iterable containing the amount of neurons per layer for
+            the actor model
+                ex: (1024, 512, 256) would make 3 fully connected layers, with
+                1024, 512 and 256 neurons respectively
+            conv_size: Iterable containing the kernel size, stride and number
+            of filters for each
+                       convolutional layer.
+                ex: ((8, 4, 16)) would make 1 convolution layer with an 8x8
+                kernel,(4, 4) stride and 16 filters
+        """
         super().__init__()
 
         # Get true input_size
@@ -45,13 +74,40 @@ class SACQNet(tf.keras.model.Model):
 
         return out
 
-class SACActor(tf.keras.models.Model):
+class SACActor(Model):
+    """
+    Soft Actor Critic Actor Model
+
+    Attributes:
+        state_size: State size the model will accept
+        convs: Convolutional layers of the model
+        flatten: Flatten operation of the model
+        fc: List containing the actor's fully connected layers
+        out: Output layer of the actor
+    """
     def __init__(self,
                  state_size=None,
                  stack_size=None,
                  action_space=None,
                  fc=None,
                  conv_size=None):
+        """
+        Constructor.
+
+        Parameters:
+            state_size: List containing the expected size of the state
+            stack_size: Numer of states we can expect in our stack
+            action_space: Box action space the actor will work in
+            fc: Iterable containing the amount of neurons per layer for
+            the actor model
+                ex: (1024, 512, 256) would make 3 fully connected layers, with
+                1024, 512 and 256 neurons respectively
+            conv_size: Iterable containing the kernel size, stride and number
+            of filters for each
+                       convolutional layer.
+                ex: ((8, 4, 16)) would make 1 convolution layer with an 8x8
+                kernel,(4, 4) stride and 16 filters
+        """
         super().__init__()
 
         # Get true input_size
@@ -91,157 +147,10 @@ class SACActor(tf.keras.models.Model):
 
         return mean, std
 
-class SACModel(tf.keras.models.Model):
-    """
-    Soft Actor Critic model.
-     
-    Attributes:
-        state_size: State size the model will accept
-        convs: Convolutional layers of the model
-        flatten: Flatten operation of the model
-        actor_fc: List containing the actor's fully connected layers
-        actor_out: Output layer of the actor
-        q1/q2_fc: Fully connected layers for each q network
-        q1/q2_out: Output layer for each q network
-    """
-    def __init__(self,
-                 state_size=None,
-                 stack_size=None,
-                 action_space=None,
-                 actor_fc=None,
-                 critic_fc=None,
-                 conv_size=None):
-        """
-        Constructor.
-
-        Parameters:
-            state_size: List containing the expected size of the state
-            stack_size: Numer of states we can expect in our stack
-            action_space: Box action space the actor will work in
-            actor_fc: Iterable containing the amount of neurons per layer for
-            the actor model
-            critic_fc: Iterable containing the amount of neurons per layer for
-            the critic model
-                ex: (1024, 512, 256) would make 3 fully connected layers, with
-                1024, 512 and 256 neurons respectively
-            conv_size: Iterable containing the kernel size, stride and number
-            of filters for each
-                       convolutional layer.
-                ex: ((8, 4, 16)) would make 1 convolution layer with an 8x8
-                kernel,(4, 4) stride and 16 filters
-        """
-        super().__init__()
-
-        # Get true input_size
-        self.state_size = state_size[:-1] + [state_size[-1] * stack_size]
-
-        # Build convolutional layers
-        if conv_size is not None:
-            if isinstance(conv_size, tuple):
-                self.convs = Custom_Convs(conv_size)
-            elif conv_size == "quake":
-                self.convs = Quake_Block()
-            else: raise ValueError("Invalid CNN Topology")
-            self.flatten = layers.Flatten()
-        else: self.convs = None
-        
-        # Build the layers for the actor and critic models
-        self.actor_fc = [layers.Dense(neurons, activation="relu",
-                                      name=f"actor_dense_{i}")
-                         for i,(neurons) in enumerate(actor_fc)]
-        self.actor_mean = layers.Dense(len(action_space), name='actor_mean')
-        self.actor_std = layers.Dense(len(action_space), name='actor_std')
-
-        # Make Q networks
-        self.q1_fc, self.q1_out = self.make_q(critic_fc, "relu", "q1")
-        self.q2_fc, self.q2_out = self.make_q(critic_fc, "relu", "q2")
-        # Make Q target networks 
-        self.q1_t_fc, self.q1_t_out = self.make_q(critic_fc, "relu", "q1_t")
-        self.q2_t_fc, self.q2_t_out = self.make_q(critic_fc, "relu", "q2_t")
-
-    def make_q(self, critic_fc, activation, name):
-        """
-        Helper function to make Q networks
-        """
-        q_fc = [layers.Dense(neurons, activation="activation",
-                                   name=f"{name}_dense_{i}") for i,(neurons)
-                      in enumerate(critic_fc)]
-        q_out = layers.Dense(1, name=f"{name}_out")
-        return q_fc, q_out
-
-    def call(self, obs, actions):
-        # Run convs on input
-        if self.convs is not None:
-            conv_out = self.convs(obs)
-            dense_in = self.flatten(conv_out)
-        else:
-            dense_in = obs
-
-        # Run actor layers
-        actor_dense = dense_in
-        for l in self.actor_fc:
-            actor_dense = l(actor_dense)
-        actor_mean = self.actor_mean(actor_dense)
-        actor_std = self.actor_std(actor_dense)
-
-        # Run critic layers
-        q1_dense = tf.concat([dense_in, actions], axis=-1)
-        for l in self.q1_fc:
-            q1_dense = l(q1_dense)
-        q1_out = q1_out(q1_dense)
-
-        q2_dense = tf.concat([dense_in, actions], axis=-1)
-        for l in self.q2_fc:
-            q2_dense = l(q2_dense)
-        q2_out = q2_out(q2_dense)
-
-        return actor_mean, actor_std, q1_out, q2_out
-
-    def step(self, obs):
-        # Run convs on input
-        if self.convs is not None:
-            conv_out = self.convs(obs)
-            dense_in = self.flatten(conv_out)
-        else:
-            dense_in = obs
-
-        # Run actor layers
-        actor_dense = dense_in
-        for l in self.actor_fc:
-            actor_dense = l(actor_dense)
-        actor_mean = self.actor_mean(actor_dense)
-        actor_std = self.actor_std(actor_dense)
-
-        return actor_mean, actor_std
-
-    def targets(self, obs, actions):
-        # Run convs on input
-        if self.convs is not None:
-            conv_out = self.convs(obs)
-            dense_in = self.flatten(conv_out)
-        else:
-            dense_in = obs
-
-        # Run target critics
-        q1_t_dense = tf.concat([dense_in, actions], axis=-1)
-        for l in self.q1_t_fc:
-            q1_t_dense = l(q1_t_dense)
-        q1_t_out = q1_t_out(q1_t_dense)
-
-        q2_t_dense = tf.concat([dense_in, actions], axis=-1)
-        for l in self.q2_t_fc:
-            q2_t_dense = l(q2_t_dense)
-        q2_t_out = q2_t_out(q2_t_dense)
-
-        return  q1_t_out, q2_t_out
-
-    def process_inputs(self, inputs):
-        # Convert n_envs x n_inputs list to n_inputs x n_envs list if we have
-        # multiple inputs
-        inputs = [np.asarray(l) for l in zip(*inputs)]
-        return inputs
-
 class Custom_Convs(tf.keras.Model):
+    """
+    Custom Convolution Block
+    """
     def __init__(self, conv_size, actv="relu"):
         super().__init__(name='')
 
@@ -258,17 +167,25 @@ class Custom_Convs(tf.keras.Model):
             x = conv(x)
         return x
 
-# Quake 3 Deepmind style convolutions
-# Like dopamine but with an additional 3x3 kernel, and skip connections
 class Quake_Block(tf.keras.Model):
+    """
+    Quake Block
+
+    Convolutions used by Deepmind for Quake. Like original Nature CNN but uses
+    more filters and skip connections.
+    """
     def __init__(self):
         super().__init__(name='')
 
-        self.conv2A = layers.Conv2D(padding="same", kernel_size=8, strides=4, filters=32, activation="relu")
-        self.conv2B = layers.Conv2D(padding="same", kernel_size=4, strides=2, filters=64, activation="relu")
-        self.conv2C = layers.Conv2D(padding="same", kernel_size=3, strides=1, filters=64)
+        self.conv2A = layers.Conv2D(padding="same", kernel_size=8, strides=4,
+                                    filters=32, activation="relu")
+        self.conv2B = layers.Conv2D(padding="same", kernel_size=4, strides=2,
+                                    filters=64, activation="relu")
+        self.conv2C = layers.Conv2D(padding="same", kernel_size=3, strides=1,
+                                    filters=64)
         self.activationC = layers.Activation("relu")
-        self.conv2D = layers.Conv2D(padding="same", kernel_size=3, strides=1, filters=64)
+        self.conv2D = layers.Conv2D(padding="same", kernel_size=3, strides=1,
+                                    filters=64)
         self.activationD = layers.Activation("relu")
 
     def call(self, x):
